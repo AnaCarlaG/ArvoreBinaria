9d34e5be9903dae8861c6e12291650d79a33caa4
AnaCarlaG <acarlagomes250@gmail.com>
2021-03-21 17:50:59 -0300

    Add,Delete, Grau,Profundidade,altura,nivel e numero de nós

--- ArvoreBinaria/Arvore.cs
+++ ArvoreBinaria/Arvore.cs
@@ -7,5 +7,104 @@ namespace ArvoreBinaria
     public class Arvore
     {
         public No raiz { get; set; }
+
+        public int Delete(int key)
+        {
+            if (this.raiz.key == key)
+            {
+                if (this.raiz.filhoEsquerdo == null && this.raiz.filhoDireito == null)
+                {
+                    var objeto = this.raiz.dados;
+                    this.raiz = null;
+                    return objeto;
+                }
+                else if (this.raiz.filhoEsquerdo != null && this.raiz.filhoDireito == null)
+                {
+                    var objeto = this.raiz.dados;
+                    this.raiz = this.raiz.filhoEsquerdo;
+                    this.raiz.noPai = null;
+                    return objeto;
+                }
+                else if (this.raiz.filhoEsquerdo == null && this.raiz.filhoDireito != null)
+                {
+                    var objeto = this.raiz.dados;
+                    this.raiz = this.raiz.filhoDireito;
+                    this.raiz.noPai = null;
+                    return objeto;
+                }
+                else
+                {
+                    var sucessor = this.raiz.Sucessor();
+                    var objeto = raiz.dados;
+                    this.Delete(sucessor.key);
+                    this.raiz.key = sucessor.key;
+                    this.raiz.dados = sucessor.dados;
+                    return objeto;
+                }
+            }
+            else
+            {
+                No node = this.raiz.Consultar(key);
+                if (node.filhoEsquerdo == null && node.filhoDireito == null)
+                {
+                    var objeto = node.dados;
+                    if (node.noPai.filhoEsquerdo.key == node.key)
+                    {
+                        node.noPai.filhoEsquerdo = null;
+                    }
+                    else 
+                    {
+                        node.noPai.filhoDireito = null;
+                    }
+                    return objeto;
+                }
+                else if (node.filhoEsquerdo != null && node.filhoDireito == null)
+                {
+                    var objeto = node.dados;
+                    if (node.noPai.filhoEsquerdo.key == node.key)
+                    {
+                        node.noPai.filhoEsquerdo = node.filhoEsquerdo;
+                    }
+                    else
+                    {
+                        node.noPai.filhoDireito = node.filhoEsquerdo;
+                    }
+                    return objeto;
+                }
+                else if (node.filhoEsquerdo == null && node.filhoDireito != null)
+                {
+                    var objeto = node.dados;
+                    if (node.noPai.filhoDireito.key == node.key)
+                    {
+                        node.noPai.filhoDireito = node.filhoDireito;
+                    }
+                    else
+                    {
+                        node.noPai.filhoEsquerdo = node.filhoDireito;
+                    }
+                    return objeto;
+                }
+                else if(node.filhoEsquerdo != null && node.filhoDireito != null)
+                {
+                    var sucessor = node.Sucessor();
+                    var objeto = node.dados;
+                    this.Delete(sucessor.key);
+                    node.key = sucessor.key;
+                    node.dados = sucessor.dados;
+                    return objeto;
+                }
+                else
+                {
+                    if (node.noPai.filhoDireito.key == node.key)
+                    {
+                        node.noPai.filhoDireito = null;
+                    }
+                    else
+                    {
+                        node.noPai.filhoEsquerdo = null;
+                    }
+                }
+            }
+        }
     }
 }
--- ArvoreBinaria/No.cs
+++ ArvoreBinaria/No.cs
@@ -7,30 +7,207 @@ namespace ArvoreBinaria
     public class No
     {
         public int dados { get; set; }
+        public int key { get; set; }
         public No filhoEsquerdo { get; set; }
         public No filhoDireito { get; set; }
         public No noPai { get; set; }
 
-        public No(int _dados, No _noPai)
+        public No(int key, int _dados, No _noPai)
         {
+            this.key = key;
             this.dados = _dados;
             this.noPai = _noPai;
         }
 
-        public bool isRaiz() 
+        public bool isRaiz()
         {
-            if (noPai != null) 
+            if (noPai != null)
             {
                 return false;
             }
             return true;
         }
 
-        private List<int> CaminhamentoNLR(List<int> lista) 
+        public void Persistir(int key, int dados)
+        {
+            this.AdicionarOrAtualizarRecursivo(key, dados);
+        }
+        private void AdicionarOrAtualizarRecursivo(int key, int dados)
+        {
+            if (this.key == key)
+            {
+                this.dados = dados;
+            }
+            else if (this.key < key)
+            {
+                if (this.filhoEsquerdo != null)
+                {
+                    filhoEsquerdo.AdicionarOrAtualizarRecursivo(key, dados);
+                }
+                else
+                {
+                    filhoEsquerdo = new No(key, dados, this);
+                }
+            }
+            else
+            {
+                if (this.filhoDireito != null)
+                {
+                    filhoDireito.AdicionarOrAtualizarRecursivo(key, dados);
+                }
+                else
+                {
+                    filhoDireito = new No(key, dados, this);
+                }
+            }
+        }
+        public No Consultar(int key)
+        {
+            return this.ConsultaRecursiva(key);
+        }
+        private No ConsultaRecursiva(int key)
+        {
+            if (this.key == key)
+            {
+                return this;
+            }
+            else if (this.key < key)
+            {
+                if (this.filhoEsquerdo != null)
+                {
+                    return this.filhoEsquerdo.ConsultaRecursiva(key);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                if (this.filhoDireito != null)
+                {
+                    return this.filhoDireito.ConsultaRecursiva(key);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        public No Sucessor()
+        {
+            if (this.filhoDireito != null)
+            {
+                return this.filhoDireito.Minimo();
+            }
+            return this;
+        }
+        public No Antecessor()
+        {
+            if (this.filhoEsquerdo != null)
+            {
+                return this.filhoEsquerdo.Maximo();
+            }
+            return this;
+        }
+        public No Maximo()
+        {
+            if (this.filhoDireito != null)
+            {
+                return this.filhoDireito.Maximo();
+            }
+            return this;
+        }
+        public No Minimo()
+        {
+            if (this.filhoEsquerdo != null)
+            {
+                return filhoEsquerdo.Minimo();
+            }
+            return this;
+        }
+        public int getGrau()
+        {
+            int aux = 1;
+            if (this.filhoEsquerdo != null)
+            {
+                aux += this.filhoEsquerdo.NumeroNos();
+            }
+            if (this.filhoDireito != null)
+            {
+                aux += filhoDireito.NumeroNos();
+            }
+            return aux;
+        }
+        public int NumeroNos()
+        {
+            int aux = 0;
+            if (isFolha())
+            {
+                return 0;
+            }
+            if (this.filhoEsquerdo != null)
+            {
+                aux += this.filhoEsquerdo.NumeroNos();
+            }
+            if (this.filhoDireito != null)
+            {
+                aux += filhoDireito.NumeroNos();
+            }
+            return aux;
+        }
+
+        public bool isFolha()
+        {
+            if (this.filhoEsquerdo == null)
+            {
+                return true;
+            }
+            else if (this.filhoDireito == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public int getProfundidade()
+        {
+            if (this.isRaiz())
+            {
+                return 0;
+            }
+            return noPai.getProfundidade() + 1;
+        }
+        public int getAltura()
+        {
+            int alturaFilhoEsquerdo = 0, alturaFilhoDireito = 0;
+            if (this.isFolha())
+            {
+                return 0;
+            }
+            if (this.filhoEsquerdo != null)
+            {
+                alturaFilhoEsquerdo = this.filhoEsquerdo.getAltura();
+            }
+            if (this.filhoDireito != null)
+            {
+                alturaFilhoDireito = this.filhoDireito.getAltura();
+            }
+            if (alturaFilhoDireito > alturaFilhoEsquerdo)
+            {
+                return alturaFilhoDireito;
+            }
+            else
+            {
+                return alturaFilhoEsquerdo;
+            }
+
+        }
+        private List<int> CaminhamentoNLR(List<int> lista)
         {
             lista.Add(this.dados);
 
-            if (this.filhoEsquerdo != null) 
+            if (this.filhoEsquerdo != null)
             {
                 this.filhoEsquerdo.CaminhamentoNLR(lista);
             }
@@ -42,7 +219,7 @@ namespace ArvoreBinaria
 
             return lista;
         }
-        public List<int> NLR() 
+        public List<int> NLR()
         {
             List<int> lista = new List<int>();
             return this.CaminhamentoNLR(lista);
